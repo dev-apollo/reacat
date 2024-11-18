dcf6bcdd2f57ce56aa672b67c78578feb1a04147
Apollo Santos <nobitagain@gmail.com>
2024-11-18 17:52:16 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div>
+      <img src='/favicon.png' style={{width:"4.5vh"}} alt="Ícone"/>
+      <main>
+        <h1>Meow!</h1>
+        <p>Ainda vai ter alguma coisa aqui... Eu espero.</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
